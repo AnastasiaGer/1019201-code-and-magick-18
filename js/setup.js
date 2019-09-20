4d8bf60205c6682cc1f80f0d0f706c6964f2bb6f
Anisa <aagermanova@gmail.com>
2019-09-19 21:54:35 -0700

    createWizard

---
+++ js/setup.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_INDEX = 3;
+
+var wizards = [];
+
+var createWizard = function (name, lastName, coatColor, eyesColor) {
+  return {
+    name: name,
+    lastName: lastName,
+    coatColor: coatColor,
+    eyesColor: eyesColor
+  };
+};
+
+var generateWizardsArray = function () {
+  for (var i = 0; i <= WIZARDS_INDEX; i++) {
+    wizards[i] = createWizard(names[i], surnames[i], coatColors[i], eyesColors[i]);
+  }
+};
+
+generateWizardsArray();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.lastName;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  similarListElement.appendChild(wizardElement);
+  return wizardElement;
+};
+
+var renderSimilarWizards = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+
+  similarListElement.appendChild(fragment);
+};
+
+renderSimilarWizards();
